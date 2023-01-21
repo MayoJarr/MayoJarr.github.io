dedf86b7e7c5863d29fbddf75c083515b69b1f75
MayoJarr <jakubmajecki2321@gmail.com>
2023-01-21 14:31:50 +0100

    cleaned code

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-// function toArray(key){
-//     if (key != 'number') console.log("error");
-//     screenNumbers.push(key);
-//     console.log(screenNumbers);
-// }
 let screenNumbers = [];
 let secondNumber = [];
-let operation = "something";
 let isSecondLine = false;
 
 const display = document.querySelector(".upperDisplay");
@@ -26,6 +20,7 @@ const multiply = document.querySelector(".multiply");
 const substract = document.querySelector(".substract");
 const add = document.querySelector(".add");
 const equal = document.querySelector(".equal");
+
 one.addEventListener("click", () => addToDisplay(1));
 two.addEventListener("click", () => addToDisplay(2));
 three.addEventListener("click", () => addToDisplay(3));
@@ -36,101 +31,63 @@ seven.addEventListener("click", () => addToDisplay(7));
 eight.addEventListener("click", () => addToDisplay(8));
 nine.addEventListener("click", () => addToDisplay(9));
 zero.addEventListener("click", () => addToDisplay(0));
-reset.addEventListener("click", () => {
-    screenNumbers = [];
-    display.textContent = "";
-    secondNumber = [];
-    secondDisplay.textContent = "";
-});
-equal.addEventListener("click", () => {
-    operate(operation, screenNumbers, secondNumber);
-    isSecondLine = false;
-});
-multiply.addEventListener("click", () => {
-    if (screenNumbers.length === 0) {
-    } else {
-        operation = "multiply";
-        display.textContent = Number(screenNumbers.join("")) + "*";
-        isSecondLine = true;
-        display.style.cssText = "opacity: .5;"
-    }
-});
-divide.addEventListener("click", () => {
-    if (screenNumbers.length === 0) {
-    } else {
-        display.style.cssText = "opacity: .5;"
-        operation = "divide";
-        display.textContent = Number(screenNumbers.join("")) + "/";
-        isSecondLine = true;
-    }
-});
-add.addEventListener("click", () => {
-    if (screenNumbers.length === 0) {
-    } else {
-        display.style.cssText = "opacity: .5;"
-        operation = "add";
-        display.textContent = Number(screenNumbers.join("")) + "+";
-        isSecondLine = true;
-    }
-});
-substract.addEventListener("click", () => {
-    if (screenNumbers.length === 0) {
-    } else {
-        display.style.cssText = "opacity: .5;"
-        operation = "substract";
-        display.textContent = Number(screenNumbers.join("")) + "-";
-        isSecondLine = true;
-    }
-});
+equal.addEventListener("click", () => operate());
+multiply.addEventListener("click", () => thing("*"));
+divide.addEventListener("click", () => thing("/"));
+add.addEventListener("click", () => thing("+"));
+substract.addEventListener("click", () => thing("-"));
+reset.addEventListener("click", () => resetThings());
 
-function addToDisplay(key) {
-    if (isSecondLine === false) {
-        screenNumbers.push(key);
-        let text = Number(screenNumbers.join(""));
-        display.textContent = text;
-        secondDisplay.textContent = "";
-        secondNumber = [];
-        if (screenNumbers.length === 13) {
-            secondDisplay.textContent = "oh fuck just dont do it, idk how to repair it,"
-            screenNumbers = [];
-            secondNumber = [];
+/* ---------prepares math --------*/
+
+function thing(operation) {
+    if (screenNumbers.length === 0) {
+    } else {
+        if (isSecondLine === true) {
+            display.style.cssText = "opacity: 1;";
+            secondDisplay.textContent = "";
         }
-    } else if (isSecondLine === true) {
-        secondNumber.push(key);
-        let text2 = Number(secondNumber.join(""));
-        secondDisplay.textContent = text2;
-        if (secondNumber.length === 13) {
-            secondDisplay.textContent = "oh fuck just dont do it, idk how to repair it"
-            secondNumber = [];
-            screenNumbers = [];
-        }
+        display.textContent = screenNumbers.join("") + operation;
+        screenNumbers.push(operation);
+        isSecondLine = false;
     }
 }
 
-function operate(operation, number1, number2) {
-    number1 = Number(number1.join(""));
-    number2 = Number(number2.join(""));
-    if (operation === "multiply") {
-        secondDisplay.textContent = number1 * number2;
-        display.textContent = number1 + "*" + number2;
-        secondNumber = [];
-        screenNumbers = [];
-        
-    } else if (operation === "divide") {
-        if (number2 === 0) {
-            secondDisplay.textContent = "ERROR";
-            display.textContent = number1 + "/" + number2;
+/*--------for reset purposes ------- */
+
+function resetThings(){
+    screenNumbers = [];
+    secondNumber = [];
+    display.textContent = "";
+    secondDisplay.textContent = "";
+}
+
+/* ---------adds numbers to screen -------*/
+
+function addToDisplay(key) {
+    if (isSecondLine === false) {
+        if (screenNumbers.length >= 13) {
+            screenNumbers.slice(0, 13);
         } else {
-            secondDisplay.textContent = number1 / number2;
-            display.textContent = number1 + "/" + number2;
+            key = String(key);
+            screenNumbers.push(key);
+            display.textContent = screenNumbers.join("");
         }
-    } else if (operation === "add") {
-        secondDisplay.textContent = number1 + number2;
-        display.textContent = number1 + "+" + number2;
-    } else if (operation === "substract") {
-        secondDisplay.textContent = number1 - number2;
-        display.textContent = number1 + "-" + number2;
+    } else if (isSecondLine === true) {
+        resetThings();
+        key = String(key);
+        screenNumbers.push(key);
+        display.textContent = screenNumbers.join("");
     }
+}
+/* -------- Does math and puts on screen --------*/
+function operate() {
+    isSecondLine = true;
+    display.textContent = screenNumbers.join("") + "=";
+    screenNumbers = eval(screenNumbers.join(""));
+    secondDisplay.textContent = screenNumbers;
+    display.style.cssText = "opacity: .5;";
+    screenNumbers = String(screenNumbers).split("");
 }
 
 /*--------------Theme switching -----------*/
@@ -138,7 +95,7 @@ function operate(operation, number1, number2) {
 const btn = document.querySelector(".btn-toggle");
 const theme = document.querySelector("#theme-link");
 
-btn.addEventListener("click", function () {
+btn.addEventListener("click", () => {
     if (theme.getAttribute("href") == "light-theme.css") {
         theme.href = "dark-theme.css";
     } else {
